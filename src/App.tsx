1eb2d9a857488e6a0fc91aea8d744e4d48331d14
kjrodriguez <kjrodriguez@credicorpcapital.com>
2024-09-17 15:46:29 -0500

    feat: add lazy loading for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
-import Home from './pages/Home';
-import Episodes from './pages/Episodes';
-import Location from './pages/Location';
+
+const Home = React.lazy(() => import ('./pages/Home'));
+const Episodes = React.lazy(() => import ('./pages/Episodes'));
+const Location = React.lazy(() => import ('./pages/Location'));
+
+const Loading = () => <div>Loading...</div>;
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/episodes" element={<Episodes />} />
-          <Route path="/location" element={<Location />} />
-        </Routes>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/episodes" element={<Episodes />} />
+            <Route path="/location" element={<Location />} />
+          </Routes>
+        </Suspense>
+
       </div>
     </Router>
   );
